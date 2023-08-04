fc4f3afbc9f349b511cbac9a1edb1040fab2e511
jingni1115 <kj227777@naver.com>
2023-08-04 19:52:44 +0900

    화면 이동 설정 (sidemenu)

--- TodoApp/TodoApp/ViewControllers/SideMenu/SideMenuViewController.swift
+++ TodoApp/TodoApp/ViewControllers/SideMenu/SideMenuViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class SideMenuViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func homeMenuTouched(_ sender: Any) {
+//        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//        if let vc: ViewController = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController {
+//            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootVC(vc, animated: false)
+//        }
     }
-    */
-
+    
+    @IBAction func completeMenuTouched(_ sender: Any) {
+        guard let rootVC = UIStoryboard.init(name: "TodoComplete", bundle: nil).instantiateViewController(withIdentifier: "TodoCompleteViewController") as? TodoCompleteViewController else {
+            return
+        }
+        let navigationController = UINavigationController(rootViewController: rootVC)
+        
+        UIApplication.shared.windows.first?.rootViewController = navigationController
+        UIApplication.shared.windows.first?.makeKeyAndVisible()
+    }
+    
+    @IBAction func trashMenuTouched(_ sender: Any) {
+        
+    }
 }
